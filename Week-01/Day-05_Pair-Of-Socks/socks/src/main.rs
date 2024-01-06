8338e183c6543f6bffcd853ee6d55d4dc9710038
Andrey Germanov <andrey@germanov.dev>
2024-01-06 10:20:32 +0300

    Day 5.

---
+++ Week-01/Day-05_Pair-Of-Socks/socks/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+fn get_num_pairs(line: &str) -> u32 {
+    let mut result: u32 = 0;
+    let mut pairs:HashMap<char, bool> = HashMap::new();
+    line.chars().for_each(|ch| {
+        if pairs.contains_key(&ch) && pairs[&ch] == true {
+            result += 1;
+            pairs.insert(ch, false);
+        } else {
+            pairs.insert(ch, true);
+        }
+    });
+    return result;
+}
+
+#[test]
+fn test_get_num_chars() {
+    assert_eq!(get_num_pairs(""), 0);
+    assert_eq!(get_num_pairs("A"), 0);
+    assert_eq!(get_num_pairs("AA"), 1);
+    assert_eq!(get_num_pairs("AAA"), 1);
+    assert_eq!(get_num_pairs("ABABC"), 2);
+    assert_eq!(get_num_pairs("CABBACCC"), 4);
+    assert_eq!(get_num_pairs("ABCDEF"), 0);
+}

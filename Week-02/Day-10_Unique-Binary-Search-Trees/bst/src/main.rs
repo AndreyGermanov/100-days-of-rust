a8ab5bae8ee796b4fb5e086662ab26a08b68c07c
Andrey Germanov <andrey@germanov.dev>
2024-01-11 13:40:22 +0300

    Day 10.

---
+++ Week-02/Day-10_Unique-Binary-Search-Trees/bst/src/main.rs
@@ -0,0 +1,11 @@
+fn bst_num(n: i32) -> i32 {
+    let numerator = (2..=n).fold(1, |accum, k| accum * (n + k));
+    let denominator = (2..=n).fold(1, |accum, k| accum * k);
+    numerator / denominator
+}
+
+#[test]
+fn test_bst_num() {
+    assert_eq!(bst_num(3), 5);
+    assert_eq!(bst_num(1), 1)
+}

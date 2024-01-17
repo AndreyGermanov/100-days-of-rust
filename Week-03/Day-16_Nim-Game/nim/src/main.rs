e7f730875001c714ea8340858c02bef35f1ca953
Andrey Germanov <andrey@germanov.dev>
2024-01-17 11:25:43 +0300

    Day 16. Cleanup.

--- Week-03/Day-16_Nim-Game/nim/src/main.rs
+++ Week-03/Day-16_Nim-Game/nim/src/main.rs
@@ -1,19 +1,19 @@
-fn do_i_win_num(n: u16) -> bool {
+fn will_i_win_nim(n: u16) -> bool {
     if n % 4 == 0 { false } else { true }
 }
 
 #[test]
 fn test_do_i_win_nim() {
-    assert_eq!(do_i_win_num(1), true);
-    assert_eq!(do_i_win_num(2), true);
-    assert_eq!(do_i_win_num(3), true);
-    assert_eq!(do_i_win_num(4), false);
-    assert_eq!(do_i_win_num(5), true);
-    assert_eq!(do_i_win_num(6), true);
-    assert_eq!(do_i_win_num(7), true);
-    assert_eq!(do_i_win_num(8), false);
-    assert_eq!(do_i_win_num(9), true);
-    assert_eq!(do_i_win_num(10), true);
-    assert_eq!(do_i_win_num(11), true);
-    assert_eq!(do_i_win_num(12), false);
+    assert_eq!(will_i_win_nim(1), true);
+    assert_eq!(will_i_win_nim(2), true);
+    assert_eq!(will_i_win_nim(3), true);
+    assert_eq!(will_i_win_nim(4), false);
+    assert_eq!(will_i_win_nim(5), true);
+    assert_eq!(will_i_win_nim(6), true);
+    assert_eq!(will_i_win_nim(7), true);
+    assert_eq!(will_i_win_nim(8), false);
+    assert_eq!(will_i_win_nim(9), true);
+    assert_eq!(will_i_win_nim(10), true);
+    assert_eq!(will_i_win_nim(11), true);
+    assert_eq!(will_i_win_nim(12), false);
 }

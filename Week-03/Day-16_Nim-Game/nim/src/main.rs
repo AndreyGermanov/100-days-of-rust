7b7eeea67ee3c52a63065d5b62c9db7f1acab57f
Andrey Germanov <andrey@germanov.dev>
2024-01-17 11:11:33 +0300

    Day 16: Refactor

--- Week-03/Day-16_Nim-Game/nim/src/main.rs
+++ Week-03/Day-16_Nim-Game/nim/src/main.rs
@@ -1,19 +1,19 @@
-fn nim(n: u16) -> bool {
+fn do_i_win_num(n: u16) -> bool {
     if n % 4 == 0 { false } else { true }
 }
 
 #[test]
-fn test_nim() {
-    assert_eq!(nim(1), true);
-    assert_eq!(nim(2), true);
-    assert_eq!(nim(3), true);
-    assert_eq!(nim(4), false);
-    assert_eq!(nim(5), true);
-    assert_eq!(nim(6), true);
-    assert_eq!(nim(7), true);
-    assert_eq!(nim(8), false);
-    assert_eq!(nim(9), true);
-    assert_eq!(nim(10), true);
-    assert_eq!(nim(11), true);
-    assert_eq!(nim(12), false);
+fn test_do_i_win_nim() {
+    assert_eq!(do_i_win_num(1), true);
+    assert_eq!(do_i_win_num(2), true);
+    assert_eq!(do_i_win_num(3), true);
+    assert_eq!(do_i_win_num(4), false);
+    assert_eq!(do_i_win_num(5), true);
+    assert_eq!(do_i_win_num(6), true);
+    assert_eq!(do_i_win_num(7), true);
+    assert_eq!(do_i_win_num(8), false);
+    assert_eq!(do_i_win_num(9), true);
+    assert_eq!(do_i_win_num(10), true);
+    assert_eq!(do_i_win_num(11), true);
+    assert_eq!(do_i_win_num(12), false);
 }

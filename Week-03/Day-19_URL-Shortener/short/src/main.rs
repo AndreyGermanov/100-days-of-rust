e7538d1617acc4fa2752c12109b5f65dfddbb329
Andrey Germanov <andrey@germanov.dev>
2024-01-20 21:06:48 +0300

    Day 19. Cleanup.

--- Week-03/Day-19_URL-Shortener/short/src/main.rs
+++ Week-03/Day-19_URL-Shortener/short/src/main.rs
@@ -16,13 +16,14 @@ struct URLS {
     long_url: String,
     short_url: Option<String>,
 }
+
 #[rocket::main]
 async fn main() {
     rocket::build()
         .manage(AppState {
             database: Mutex::new(HashMap::new()),
         })
-        .mount("/",routes![index])
+        .mount("/", routes![index])
         .mount("/generate", routes![generate])
         .mount("/f", routes![forward])
         .launch().await.unwrap();
@@ -45,35 +46,35 @@ fn generate(urls: Option<Form<URLS>>, state: &State<AppState>) -> Result<String,
             return Err(Status::BadRequest);
     }
     if state.database.lock().unwrap().iter().find(|(key,value)| *value == &long_url) != None {
-        return Err(Status::Conflict)
+        return Err(Status::Conflict);
     }
     let mut short_url = urls.short_url.clone().unwrap_or("".to_string());
     if short_url.len() == 0 {
         short_url = generate_short_for_url(&state.database.lock().unwrap(), &urls.long_url);
     } else {
         if state.database.lock().unwrap().contains_key(&short_url) {
-            return Err(Status::Conflict)
+            return Err(Status::Conflict);
         }
     };
-    state.database.lock().unwrap().insert(short_url.clone(),long_url);
-    return Ok(format!("http://localhost:8080/f/{}",short_url));
+    state.database.lock().unwrap().insert(short_url.clone(), long_url);
+    return Ok(format!("http://localhost:8080/f/{}", short_url));
 }
 
 #[get("/<code>")]
 fn forward(code: &str, state: &State<AppState>) -> Result<Redirect,Status> {
-    let url = get_url_by_short(&state.database.lock().unwrap(),code);
+    let url = get_url_by_short(&state.database.lock().unwrap(), code);
     if url.len() > 0 {
-        Ok(Redirect::to(url))
+        return Ok(Redirect::to(url));
     } else {
-        Err(Status::NotFound)
+        return Err(Status::NotFound);
     }
 }
 
-fn get_url_by_short(database: &HashMap<String,String>, short: &str) -> String {
+fn get_url_by_short(database: &HashMap<String, String>, short: &str) -> String {
     database.get(short).unwrap_or(&"".to_string()).clone()
 }
 
-fn generate_short_for_url(database: &HashMap<String,String>,url: &str) -> String {
+fn generate_short_for_url(database: &HashMap<String, String>, url: &str) -> String {
     let mut code = String::new();
     for _ in 0..8 {
         let num = rand::thread_rng().gen_range(65..123);
@@ -87,5 +88,5 @@ fn generate_short_for_url(database: &HashMap<String,String>,url: &str) -> String
     if get_url_by_short(database, url) != "" {
         return generate_short_for_url(database, url)
     }
-    return code
+    return code;
 }

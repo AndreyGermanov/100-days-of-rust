76c559292ee72ab770632147688890f7e7bc8ccd
Andrey Germanov <andrey@germanov.dev>
2024-02-04 09:53:10 +0300

    Day 34.

--- Week-05/Day-34_Primary-Arithmetic/primary/src/main.rs
+++ Week-05/Day-34_Primary-Arithmetic/primary/src/main.rs
@@ -1,13 +1,13 @@
-fn carry_operations(mut num1: i32, mut num2: i32) -> i32 {
+fn carry_operations(mut num1: u32, mut num2: u32) -> u32 {
     let mut result = 0;
-    while num2 > 0 {
+    loop {
         let num2_digit = num2 % 10;
         num2 = (num2 - num2_digit) / 10;
         let num1_digit = num1 % 10 + num2_digit;
         num1 = (num1 + num1_digit) / 10;
         if (num1_digit) > 9 { result += 1; }
+        if num2 <= 0 { break result; }
     }
-    result
 }
 
 #[test]

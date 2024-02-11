6c4490e93bc151988067574dc153081c7b1eddca
Andrey Germanov <andrey@germanov.dev>
2024-02-11 16:23:16 +0300

    Day 41.

---
+++ Week-06/Day-41_Sales-By-Match/socks/src/main.rs
@@ -0,0 +1,21 @@
+use std::collections::HashMap;
+
+fn get_num_pairs(input: &[u8]) -> u8 {
+    let mut pairs:HashMap<u8, u8> = HashMap::new();
+    input.iter().fold(0,|mut accum,item| {
+        return if pairs.get(item).unwrap_or(&0_u8) > &0_u8 {
+            pairs.insert(*item, 0); accum += 1; accum
+        } else {
+            pairs.insert(*item, 1); accum
+        }
+    })
+}
+
+#[test]
+fn test_get_num_pairs() {
+    assert_eq!(get_num_pairs(&[1, 2, 1, 2, 1, 3, 2]), 2);
+    assert_eq!(get_num_pairs(&[1, 1, 1]), 1);
+    assert_eq!(get_num_pairs(&[1]), 0);
+    assert_eq!(get_num_pairs(&[]), 0);
+    assert_eq!(get_num_pairs(&[3, 2, 3, 2, 3, 2, 3, 2, 3, 2, 3]), 5);
+}

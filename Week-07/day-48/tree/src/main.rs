9b4651d4c3615f400842f749c887ce7048c27e35
Andrey Germanov <andrey@germanov.dev>
2024-02-18 17:15:52 +0300

    Day 48

---
+++ Week-07/day-48/tree/src/main.rs
@@ -0,0 +1,22 @@
+fn tree(num:usize) -> Vec<String> {
+    (0..num).fold(vec![],|mut accum,n| {
+        let pad: String = vec![' '; num-n-1].iter().collect();
+        let symb: String = vec!['#'; 2*n+1].iter().collect();
+        accum.push(pad.to_owned() + symb.as_str() + pad.as_str()); accum
+    })
+}
+
+#[test]
+fn test_tree() {
+    assert_eq!(tree(1), vec!["#"]);
+    assert_eq!(tree(2), vec![" # ",
+                                  "###"
+    ]);
+    assert_eq!(tree(5), vec!["    #    ",
+                                  "   ###   ",
+                                  "  #####  ",
+                                  " ####### ",
+                                  "#########"
+    ]);
+    assert_eq!(tree(0), vec![] as Vec<&str>);
+}

f7e94ab18a195f818ad83998cdb16343aab35c49
Andrey Germanov <andrey@germanov.dev>
2024-03-01 10:03:41 +0300

    Day 60.

---
+++ Week-09/Day-60_A-Game-Of-Thrones/thrones/src/main.rs
@@ -0,0 +1,26 @@
+use std::io;
+use std::io::BufRead;
+use std::str::FromStr;
+
+fn main() {
+    for line in io::stdin().lock().lines() {
+        match line {
+            Ok(line) if u32::from_str(line.as_str()).is_ok() => {
+                let mut num = u32::from_str(line.as_str()).unwrap();
+                if num < 3 { continue }
+                play(num);
+                break;
+            },
+            _ => continue
+        }
+    }
+}
+
+fn play(mut num: u32) {
+    while num > 1 {
+        let op:i32 = if (num + 1) % 3 == 0 {1} else if (num - 1) % 3 == 0 {-1} else {0};
+        println!("{} {}", num, op);
+        num = (num as i32 + op) as u32 / 3;
+    }
+    println!("{}", num);
+}
